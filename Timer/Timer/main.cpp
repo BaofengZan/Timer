d22ad03a23a66f6b34ea791948b9b8c1f30373fb
BaofengZan <zanbf1123@163.com>
2020-08-25 15:16:41 +0800

    添加cmake文件

--- Timer/Timer/main.cpp
+++ Timer/Timer/main.cpp
@@ -10,7 +10,15 @@ int main()
     {
         std::cout << i << std::endl;
     }
-    float elaps = time.MicroSeconds();
-    std::cout <<"耗时： " << elaps << "ms" << std::endl; 
+    float elaps = time.MilliSeconds();
+    std::cout <<"耗时1： " << elaps << "ms" << std::endl; 
+
+    time.Start();
+    for (int i = 0; i < 1000; ++i)
+    {
+        std::cout << i << std::endl;
+    }
+    elaps = time.MilliSeconds();
+    std::cout << "耗时2： " << elaps << "ms" << std::endl;
     return 0;
 }
